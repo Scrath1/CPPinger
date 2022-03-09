ccd49a906bbdc289ffd0884a5e3ea4807070e35f
Tobias <tobiasstein1@gmx.net>
2022-03-09 11:39:52 +0100

    added event handling

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,47 @@
 using p_chan = msd::channel<icmplib::PingResult>;
 bool stop = false;
 
+static const std::string ResponseStrings[] = {"Success","Unreachable","TimeExceeded","Timeout","Unsupported","Failure"};
+
+std::string responseToString(icmplib::PingResponseType response){
+    return ResponseStrings[static_cast<int>(response)];
+}
+
 void sigint_handler(int signum){
     std::cout << "Caught stop signal" << std::endl;
     stop=true;
 }
 
-void event_handling(p_chan& in){
+void event_handling(p_chan& in, int pingWarningThreshold){
+    bool pingWarningDetected = false; // flag to determine the flank when the ping goes above/below the threshold
+    icmplib::PingResponseType lastResponse = icmplib::PingResponseType::Success;
+
+    Logger* logger = Logger::getInstance();
     while(!stop){
         icmplib::PingResult res;
         res << in;
-        std::cout << "Ping: " << res.interval << std::endl;
+
+        // Connection events
+        if(res.response!=lastResponse){ // if change in network connection occurs
+            if(lastResponse==icmplib::PingResponseType::Success){ // connection lost
+                logger->log(LogLevel::logINFO, "Lost connection", EventType::Timeout);
+            }
+            else if (res.response==icmplib::PingResponseType::Success){ // reconnected
+                logger->log(LogLevel::logINFO, "Reconnected", EventType::Timeout);
+            }
+        }
+
+        // Ping events
+        if(res.response==icmplib::PingResponseType::Success) {
+            if (res.interval > pingWarningThreshold && !pingWarningDetected) { // ping went above the threshold
+                logger->log(LogLevel::logINFO, "Ping exceeded warning threshold", EventType::High_Ping);
+                pingWarningDetected = true;
+            } else if (res.interval < pingWarningThreshold && pingWarningDetected) { // ping went back below the threshold
+                logger->log(LogLevel::logINFO, "Ping back to normal", EventType::High_Ping);
+            }
+        }
+        lastResponse = res.response;
+        std::cout << "Ping: " << res.interval << " Response: "<< responseToString(res.response) <<std::endl;
     }
 }
 
@@ -26,7 +57,7 @@ void continuous_ping(p_chan& out, const std::string& target,const int& interval 
     icmplib::IPAddress t = icmplib::IPAddress(target);
     std::cout << "Starting to ping target " << target << " with interval " << interval << std::endl;
     while (!stop){
-        icmplib::PingResult result = icmplib::Ping(t);
+        icmplib::PingResult result = icmplib::Ping(t, interval);
         result >> out;
         sleep(interval);
     }
@@ -39,6 +70,7 @@ int main() {
     int pingInterval = config["interval"].as<int>();
     std::string target = config["target"].as<std::string>();
     std::string logfileName = config["logfile_name"].as<std::string>();
+    int pingWarningThreshold = config["pingwarning_threshold"].as<int>();
 
     Logger::setFileTarget(logfileName);
     Logger* logger = Logger::getInstance();
@@ -50,7 +82,7 @@ int main() {
     p_chan pingResults;
 
     threads.emplace_back(&continuous_ping, std::ref(pingResults), std::ref(target), std::ref(pingInterval));
-    threads.emplace_back(std::thread(&event_handling, std::ref(pingResults)));
+    threads.emplace_back(std::thread(&event_handling, std::ref(pingResults), std::ref(pingWarningThreshold)));
 
     for(std::thread& t: threads){
         t.join();
